bb4117126be3f8729398922d286cefbf7878b153
Vasyl Liutikov <pingwinator@gmail.com>
2019-01-31 21:20:03 +0100

    Initial Commit

---
+++ EmptyTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  EmptyTest
+//
+//  Created by Vasyl Liutikov on 31.01.19.
+//  Copyright © 2019 pingwinator. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ EmptyTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  EmptyTest
+//
+//  Created by Vasyl Liutikov on 31.01.19.
+//  Copyright © 2019 pingwinator. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
